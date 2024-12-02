d1e3ee2a030aa29b3ec5da2e416a6cbded9c19f9
AlbertGStanica <nois3less@gmail.com>
2024-12-02 09:56:03 -0400

    btnRollDice included
    Added up to and including the RollDice_Click event

--- Lab5/Form1.cs
+++ Lab5/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,31 +17,34 @@ namespace Lab5
         {
             InitializeComponent();
         }
-        /* Name:
+        /* Name: Albert Stanica
          * Date: November 2024
          * This program rolls one dice or calculates mark stats.
-         * Link to your repo in GitHub: 
+         * Link to your repo in GitHub: https://github.com/AlbertGStanica/StanicaLab5
          * */
 
         //class-level random object
         Random rand = new Random();
+        private const String NAME = "Albert Stanica";
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //select one roll radiobutton
-            
+            radOneRoll.Checked = true;
             //add your name to end of form title
-            
+            this.Text += NAME;
         } // end form load
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             //call the function
+            ClearOneRoll();
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
             //call the function
+            ClearStats();
             
         }
 
@@ -48,33 +52,52 @@ namespace Lab5
         {
             int dice1, dice2;
             //call ftn RollDice, placing returned number into integers
-            
+            dice1 = RollDice();
+            dice2 = RollDice();
             //place integers into labels
-            
+            lblDice1.Text = dice1 + "";
+            lblDice2.Text = dice2 + string.Empty;
             // call ftn GetName sending total and returning name
-
+            int total = dice1 + dice2;
+            string rollName = GetName(total);
             //display name in label
-            
+            lblRollName.Text = rollName;
         }
 
         /* Name: ClearOneRoll
         *  Sent: nothing
         *  Return: nothing
         *  Clear the labels */
-
+        private void ClearOneRoll()
+        {
+            lblDice1.Text = "";
+            lblDice2.Text = "";
+            lblRollName.Text = "";
+        }
 
         /* Name: ClearStats
         *  Sent: nothing
         *  Return: nothing
         *  Reset nud to minimum value, chkbox unselected, 
         *  clear labels and listbox */
-
+        private void ClearStats()
+        {
+            nudNumber.Value = nudNumber.Minimum;
+            chkSeed.Checked = false;
+            lblPass.Text = "";
+            lblFail.Text = "";
+            lblAverage.Text = "";
+            lstMarks.Items.Clear();
+        }
 
         /* Name: RollDice
         * Sent: nothing
         * Return: integer (1-6)
         * Simulates rolling one dice */
-
+        private int RollDice()
+        {
+            return rand.Next(1 , 7);
+        }
 
         /* Name: GetName
         * Sent: 1 integer (total of dice1 and dice2) 
@@ -89,7 +112,38 @@ namespace Lab5
         *        11 = Yo-leven
         *        12 = Boxcars
         * Anything else = No special name*/
-
+        private string GetName(int total)
+        {
+            string rollName = "";
+            switch (total)
+            {
+                case 2:
+                    rollName = "Snake Eyes";
+                    break;
+                case 3:
+                    rollName = "Litle Joe";
+                    break;
+                case 5:
+                    rollName = "Fever";
+                    break;
+                case 7:
+                    rollName = "Most Common";
+                    break;
+                case 9:
+                    rollName = "Center Field";
+                    break;
+                case 11:
+                    rollName = "Yo-level";
+                    break;
+                case 12:
+                    rollName = "Boxcars";
+                    break;
+                default:
+                    rollName = "";
+                    break;
+            }
+            return rollName;
+        }
         private void btnSwapNumbers_Click(object sender, EventArgs e)
         {
             //call ftn DataPresent twice sending string returning boolean
@@ -126,6 +180,22 @@ namespace Lab5
             // Format average always showing 2 decimal places 
 
         } // end Generate click
+
+        private void radOneRoll_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radOneRoll.Checked)
+            {
+                grpMarkStats.Hide();
+                grpOneRoll.Show();
+                ClearStats();
+            }
+            else
+            {
+                grpOneRoll.Hide();
+                grpMarkStats.Show();
+                ClearOneRoll();
+            }
+        }
 
         /* Name: CalcStats
         * Sent: array and 2 integers
